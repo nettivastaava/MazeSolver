59031bc629f83bacbc11367ce1c02a4a4cf53ff5
nettivastaava <vaxandst@cubbli20-gold.local>
2021-02-04 15:51:01 +0200

    BFS tests started

---
+++ maze-solver/src/test/java/BFSTest.java
@@ -0,0 +1,54 @@
+import mazesolver.domain.Maze;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.io.File;
+import java.io.FileWriter;
+import mazesolver.domain.BreadthFirst;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class BFSTest {
+    @Rule   
+        public TemporaryFolder testFolder = new TemporaryFolder();
+        Maze maze;
+        BreadthFirst bfs;
+        File asciiFile;
+        
+        @Before
+        public void setUp() throws Exception {
+            asciiFile = testFolder.newFile("asciiFile.txt");
+            
+            try (FileWriter file = new FileWriter(asciiFile.getAbsolutePath())) {
+                file.write("@@@@@@@@@\n");
+                file.write("@S......@\n");
+                file.write("@.@@.@@.@\n");
+                file.write("@.@@@@@.@\n");
+                file.write("@..F@@..@\n");
+                file.write("@@@@@@@@@\n");
+            }
+            
+            
+            
+            maze = new Maze(6, 9, asciiFile.getAbsolutePath());
+            bfs = new BreadthFirst();
+        }
+        
+        @Test
+        public void correctPathIsFound() {
+            assertEquals("AAAOO", bfs.bfs(maze.getMaze()));
+        }
+        
+        @Test
+        public void returnsNullWhenNoPathFound() {
+            maze.getMaze()[1][2]='.';
+            assertEquals(bfs.bfs(maze.getMaze()), null);
+        }
+        
+        @After
+        public void tearDown() {
+            asciiFile.delete();
+        }
+}
