d97821905f9daa775b6979a4c1596b180dafe044
nettivastaava <vaxandst@cubbli20-gold.local>
2021-02-26 16:55:13 +0200

    UI done

--- maze-solver/src/main/java/mazesolver/ui/Main.java
+++ maze-solver/src/main/java/mazesolver/ui/Main.java
@@ -4,27 +4,81 @@ import mazesolver.domain.BreadthFirst;
 import mazesolver.domain.Maze;
 import mazesolver.domain.Wallfollower;
 import mazesolver.domain.DeadendFiller;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) { 
+        Scanner s = new Scanner(System.in);
+        Wallfollower wf = new Wallfollower();
+        BreadthFirst bfs = new BreadthFirst();
+        DeadendFiller df = new DeadendFiller();
         
-        Maze m = new Maze(40, 60, "maze6.txt");
-                
-        m.printMaze();
-        BreadthFirst bfs1 = new BreadthFirst();
-        Wallfollower wf1 = new Wallfollower();
-        DeadendFiller df1 = new DeadendFiller();
-        
-     
-        
-        double start = System.nanoTime();
-        System.out.println("BFS: " + bfs1.bfs(m.getMaze()));
-        double end = System.nanoTime();
-        double elapsed = end - start;
-        System.out.println(elapsed);
-        System.out.println("");
-        
-  
+        while(true) {
+            System.out.println("Number of rows:");
+            int rows = Integer.valueOf(s.nextLine());
+            System.out.println("Number of columns:");
+            int columns = Integer.valueOf(s.nextLine());
+            System.out.println("Name of the file:");
+            String name = s.nextLine();
+
+            Maze m = new Maze(rows, columns, name);
+
+            m.printMaze();
+
+            System.out.println("Choose the algorithm used to solve the maze");
+            System.out.println("1: Wall follower");
+            System.out.println("2: Breadth first");
+            System.out.println("3: Dead end filler (simply connected mazes only)");
+            System.out.println("4: Dead end filler with breadth first");
+
+            int algorithm = Integer.valueOf(s.nextLine());
+
+            if (algorithm == 1) {
+                long start = System.nanoTime();
+                String path = wf.searchPath(m.getMaze());
+                System.out.println("WF: " + path + " " + path.length());
+                long end = System.nanoTime();
+                long elapsed = end - start;
+                System.out.println(elapsed/1e9);
+                System.out.println("");
+            } else if (algorithm == 2) {
+                long start = System.nanoTime();
+                String path = bfs.bfs(m.getMaze());
+                System.out.println("BFS: " + path + " " + path.length());
+                long end = System.nanoTime();
+                long elapsed = end - start;
+                System.out.println(elapsed/1e9);
+                System.out.println("");
+            } else if (algorithm == 3) {
+                long start = System.nanoTime();
+                String path = df.defCompleteMaze(m.getMaze());
+                System.out.println("DEF: " + path + " " + path.length());
+                long end = System.nanoTime();
+                long elapsed = end - start;
+                System.out.println(elapsed/1e9);
+                System.out.println("");
+            } else if (algorithm == 4) {
+                long start = System.nanoTime();
+                String path = df.defWithBFS(m.getMaze());
+                System.out.println("DEF: " + path + " " + path.length());
+                long end = System.nanoTime();
+                long elapsed = end - start;
+                System.out.println(elapsed/1e9);
+                System.out.println("");
+            } else {
+                System.out.println("Invalid input");
+                System.out.println("");
+            }
+            
+            System.out.println("Solve another one? (type 'q' to quit or anything else to continue)");
+            String quit = s.nextLine();
+            
+            if (quit.equals("q")) {
+                break;
+            }
+            
+            
+        }
         
     }
 }
